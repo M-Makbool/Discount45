09161c273a3809eb08a2739b2669b88b12ec4c17
Mohammed Makbool <M.Makbool@outlook.sa>
2025-05-19 22:11:16 +0300

    add Oracle database object and sql ddl querys

--- src/main/scala/Project/Discount45.scala
+++ src/main/scala/Project/Discount45.scala
@@ -12,7 +12,7 @@ object Discount45 extends App {
   // Reads all transaction lines from the CSV file, skipping the header.
   val lines: List[String] = Source.fromFile("src/main/resources/TRX1000.csv").getLines().toList.tail
 
-  lines.map(toTransaction).map(addDiscount).foreach(println)
+    lines.map(toTransaction).map(addDiscount).foreach(OracleDB.write)
 
   /**
    * Applies all discount rules to a transaction, selects the top two discounts,
